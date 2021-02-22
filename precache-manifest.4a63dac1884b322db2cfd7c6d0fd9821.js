0e85ed272ffe7d266fa8d4db016d952627e8c3fd
Hunter McGregor <yatamare@outlook.com>
2021-02-21 18:56:38 -0600

    Updates

---
+++ precache-manifest.4a63dac1884b322db2cfd7c6d0fd9821.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13159a80b892cf13068dad126100000a",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "da117b4b2197cf5ac31e",
+    "url": "/React-Portfolio/static/css/2.799c978e.chunk.css"
+  },
+  {
+    "revision": "6d1750f9278f9fe0c709",
+    "url": "/React-Portfolio/static/css/main.113c32c6.chunk.css"
+  },
+  {
+    "revision": "da117b4b2197cf5ac31e",
+    "url": "/React-Portfolio/static/js/2.2f393919.chunk.js"
+  },
+  {
+    "revision": "f54a3fe436bfb6b2e270f79332227879",
+    "url": "/React-Portfolio/static/js/2.2f393919.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1750f9278f9fe0c709",
+    "url": "/React-Portfolio/static/js/main.351ac03e.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  },
+  {
+    "revision": "17ea7bf7a9aa8541e7fc231f8214a5c1",
+    "url": "/React-Portfolio/static/media/4m1.17ea7bf7.png"
+  },
+  {
+    "revision": "3550e08da4a555cd04178b792c3bb61a",
+    "url": "/React-Portfolio/static/media/4m1.3550e08d.png"
+  },
+  {
+    "revision": "6580f38ec7f5ab8a50bdee5ee3edccbc",
+    "url": "/React-Portfolio/static/media/4m3.6580f38e.png"
+  },
+  {
+    "revision": "9ba3e021a4ad8323de9910fcab0e6af2",
+    "url": "/React-Portfolio/static/media/4m3.9ba3e021.png"
+  },
+  {
+    "revision": "24ce73a6da5eafefd54702b460fb68c7",
+    "url": "/React-Portfolio/static/media/4mold.24ce73a6.png"
+  },
+  {
+    "revision": "862ee95d8250247b16985727bbed6177",
+    "url": "/React-Portfolio/static/media/Bath.862ee95d.png"
+  },
+  {
+    "revision": "60021c367ab6ebf12b3d3ad221f19308",
+    "url": "/React-Portfolio/static/media/Cali.60021c36.png"
+  },
+  {
+    "revision": "9a178d40a37a3a64241b0b1616717b82",
+    "url": "/React-Portfolio/static/media/Cali.9a178d40.png"
+  },
+  {
+    "revision": "74b61c195ba841fae070b59762932b13",
+    "url": "/React-Portfolio/static/media/Flowers.74b61c19.png"
+  },
+  {
+    "revision": "f6e9b8dd3d1f9bfcc6b84679a96f61cf",
+    "url": "/React-Portfolio/static/media/Flowers.f6e9b8dd.png"
+  },
+  {
+    "revision": "89927debaa800100c68c260255e38ce5",
+    "url": "/React-Portfolio/static/media/Light.89927deb.png"
+  },
+  {
+    "revision": "a2bf32517d2f62212b00016061a55692",
+    "url": "/React-Portfolio/static/media/Light.a2bf3251.png"
+  },
+  {
+    "revision": "6b0f3954e7ea726610111c1fcc145abc",
+    "url": "/React-Portfolio/static/media/SolGame.6b0f3954.png"
+  },
+  {
+    "revision": "a0efd29e10a905bee452b42a73370bab",
+    "url": "/React-Portfolio/static/media/SolHouse.a0efd29e.png"
+  },
+  {
+    "revision": "9359be0b6fbeed78d28760e6148077b0",
+    "url": "/React-Portfolio/static/media/SolTown.9359be0b.png"
+  },
+  {
+    "revision": "ae9d9f09b5c6c61a9ae1db00173a2caa",
+    "url": "/React-Portfolio/static/media/SolTown.ae9d9f09.png"
+  },
+  {
+    "revision": "94b7003426225e6dc40ee3b876a877cf",
+    "url": "/React-Portfolio/static/media/Starexplorer.94b70034.png"
+  },
+  {
+    "revision": "c4d753e098b76d697264d04fb23258b3",
+    "url": "/React-Portfolio/static/media/Upsidedown.c4d753e0.png"
+  },
+  {
+    "revision": "3a5ecbde239557316daf39927d37b5ca",
+    "url": "/React-Portfolio/static/media/Uptownwebsite.3a5ecbde.png"
+  },
+  {
+    "revision": "578976de30582c8f021d00bc6d923cc8",
+    "url": "/React-Portfolio/static/media/Written.578976de.png"
+  },
+  {
+    "revision": "0c9bcd48a7bce8e87a75faa1faeeabad",
+    "url": "/React-Portfolio/static/media/dotaSuggestions.0c9bcd48.png"
+  },
+  {
+    "revision": "6f5baac380895b6f053378fcdd0012c8",
+    "url": "/React-Portfolio/static/media/dotaSuggestions.6f5baac3.png"
+  },
+  {
+    "revision": "3ee0259c9c6a59493cba0d185f79a599",
+    "url": "/React-Portfolio/static/media/hyperstone.3ee0259c.png"
+  },
+  {
+    "revision": "497757f5d66becf726cfbc98cec1d870",
+    "url": "/React-Portfolio/static/media/hyperstone.497757f5.png"
+  },
+  {
+    "revision": "12d8fd3d7f81df83aab5a58c48a30c43",
+    "url": "/React-Portfolio/static/media/pit2.12d8fd3d.png"
+  },
+  {
+    "revision": "9a6f0afac3ab56162a9e855a5c58e8f1",
+    "url": "/React-Portfolio/static/media/pit2.9a6f0afa.png"
+  },
+  {
+    "revision": "202c381e53f13c9916306892e597f26e",
+    "url": "/React-Portfolio/static/media/pycode.202c381e.png"
+  }
+]);
